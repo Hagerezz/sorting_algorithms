c39006d034869095050ba50c9b84fae8a4b1a41d
hagerezz <queen.emilly12@gmail.com>
2023-07-30 05:59:50 +0300

    hi

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,37 +2,36 @@
 
 /**
  * insertion_sort_list: insertion sort
- * list: double array
+ * @list: double array
 */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node = NULL;
-    listint_t *prev = NULL;
-    listint_t *next = NULL;
+	listint_t *node = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-    if (list == NULL || *list == NULL)
-        return;
-    node = (*list)->next;
-    while (node != NULL)
-    {
-        next = node->next;
-        prev = node->prev;
-        while (prev != NULL && prev->n > node->n)
-        {
-            if (node->next != NULL)
-                node->next->prev = prev;
-            if (prev->prev != NULL)
-                prev->prev->next = node;
-            else
-                *list = node;
-            
-            prev->next = node->next;
-            node->prev = prev->prev;
-            node->next = prev;
-            prev->prev = node;
-            print_list(*list);
-            prev = node->prev;
-        }
-        node = next;   
-    }
+	if (list == NULL || *list == NULL)
+		return;
+	node = (*list)->next;
+	while (node != NULL)
+	{
+		next = node->next;
+		prev = node->prev;
+		while (prev != NULL && prev->n > node->n)
+		{
+			if (node->next != NULL)
+				node->next->prev = prev;
+			if (prev->prev != NULL)
+				prev->prev->next = node;
+			else
+				*list = node;
+			prev->next = node->next;
+			node->prev = prev->prev;
+			node->next = prev;
+			prev->prev = node;
+			print_list(*list);
+			prev = node->prev;
+		}
+		node = next;
+	}
 }
